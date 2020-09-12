770e50d1fe7b2fde063ca076f93fd9c56f07a4c0
geir <geireilertsen@hotmail.com>
2020-09-12 12:49:14 +0200

    Oppg2 code complete

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import './styles.scss'
+import './styles.scss';
 
-const app = document.getElementById("app");
+const app = document.getElementById('app');
 
-app.innerHTML = `
-<h1>Hello Webpack!</h1>
-`;
+app.innerHTML = '<h1>Hello Webpack!</h1>';
+app.innerHTML += '<button id="btn">Test</button>';
 
+const btn = document.getElementById('btn');
 
-
-
+btn.addEventListener('click', function () {
+  app.innerHTML += '<p>SHIIIIIT</p>';
+});
